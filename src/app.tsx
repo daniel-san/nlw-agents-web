ce53e29677a18853db094f09dd05c87b59deb2f7
Daniel Borges <dlsbdaniel@gmail.com>
2025-07-11 16:59:55 -0300

    feat: app setup and rooms listing

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from '@/pages/create-room.tsx';
+import { Room } from '@/pages/room.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
