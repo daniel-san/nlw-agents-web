e7d216bbefe000e6befed5c6f3d3599b727e0d0d
Daniel Borges <dlsbdaniel@gmail.com>
2025-07-26 10:15:03 -0300

    feat: add page and code to record voice through browser

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CreateRoom } from '@/pages/create-room.tsx';
 import { Room } from '@/pages/room.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecordRoomAudio } from '@/pages/record-room-audio.tsx';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ export function App() {
         <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
